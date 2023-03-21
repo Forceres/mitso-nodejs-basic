1055e07bbda527745a48bc362023f937e4f74957
Forceres <ilya.sereda.2001@gmail.com>
2023-03-21 10:27:34 +0300

    Prettier config applied

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,22 +1,22 @@
-import { createHash } from "crypto";
-import { createReadStream } from "fs";
-import { join } from "path";
-import { fileURLToPath, URL } from "url";
+import { createHash } from 'crypto'
+import { createReadStream } from 'fs'
+import { join } from 'path'
+import { fileURLToPath, URL } from 'url'
 
-const __dirname = fileURLToPath(new URL(".", import.meta.url));
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 const calculateHash = async () => {
-  const hash = createHash("SHA256");
-  const readable = createReadStream(
-    join(__dirname, "files", "fileToCalculateHashFor.txt")
-  );
-  readable.on("readable", () => {
-    const data = readable.read();
-    if (data) hash.update(data);
-    else {
-      console.log(`${hash.digest("hex")}`);
-    }
-  });
-};
+    const hash = createHash('SHA256')
+    const readable = createReadStream(
+        join(__dirname, 'files', 'fileToCalculateHashFor.txt')
+    )
+    readable.on('readable', () => {
+        const data = readable.read()
+        if (data) hash.update(data)
+        else {
+            console.log(`${hash.digest('hex')}`)
+        }
+    })
+}
 
-await calculateHash();
+await calculateHash()
