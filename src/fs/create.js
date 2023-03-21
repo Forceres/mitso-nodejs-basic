75d4d3746ce857df92b094b9e2139d3d9cf0bf5c
Forceres <forceres31@gmail.com>
2023-03-21 10:27:34 +0300

    Prettier config applied

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,26 +1,26 @@
-import { access, writeFile } from "fs";
-import { join } from "path";
-import { fileURLToPath, URL } from "url";
+import { access, writeFile } from 'fs'
+import { join } from 'path'
+import { fileURLToPath, URL } from 'url'
 
-const __dirname = fileURLToPath(new URL(".", import.meta.url));
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
-const str = "I am fresh and young";
+const str = 'I am fresh and young'
 
 const writeFilePromise = function () {
-  return new Promise((_resolve, reject) => {
-    access(join(__dirname, "files", "fresh.txt"), (err) => {
-      if (!err) reject();
-    });
-    writeFile(join(__dirname, "files", "fresh.txt"), str, (err) => {
-      if (err) reject("Something wrong with writing a file");
-    });
-  });
-};
+    return new Promise((_resolve, reject) => {
+        access(join(__dirname, 'files', 'fresh.txt'), (err) => {
+            if (!err) reject()
+        })
+        writeFile(join(__dirname, 'files', 'fresh.txt'), str, (err) => {
+            if (err) reject('Something wrong with writing a file')
+        })
+    })
+}
 
 const create = async () => {
-  await writeFilePromise().catch((err) => {
-    throw new Error("FS operation failed");
-  });
-};
+    await writeFilePromise().catch((err) => {
+        throw new Error('FS operation failed')
+    })
+}
 
-await create();
+await create()
