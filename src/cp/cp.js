4b304c50dd6474443fd23717e34e2314548af250
Ilya <ilya.sereda.2004@mail.ru>
2023-03-22 16:24:58 +0300

    Fix .prettierrc.yaml

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,23 +1,14 @@
-import { join } from 'path'
-import { fileURLToPath, URL } from 'url'
-import { fork } from 'child_process'
-import { stdin } from 'process'
+import { join } from 'path';
+import { fileURLToPath, URL } from 'url';
+import { fork } from 'child_process';
+import { stdin } from 'process';
 
-const __dirname = fileURLToPath(new URL('.', import.meta.url))
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 const spawnChildProcess = async (args) => {
-    const forked = fork(join(__dirname, 'files', 'script.js'), args, {
-        stdio: [0, 1, 2, 'ipc'],
-    })
-    forked.on('message', (msg) => {
-        console.log(msg.trim())
-    })
-    forked.on('disconnect', () => {
-        process.exit(0)
-    })
-    stdin.on('data', (data) => {
-        forked.send(data.toString())
-    })
-}
+  fork(join(__dirname, 'files', 'script.js'), args, {
+    stdio: [0, 1, 2, 'ipc'],
+  });
+};
 // Put your arguments in function call to test this functionality
-spawnChildProcess(['Maksim', 'Aleksandrovich', 'Kalinin'])
+spawnChildProcess(['Maksim', 'Aleksandrovich', 'Kalinin']);
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,17 +1,16 @@
-import fsPromise from 'fs/promises'
-import { join } from 'path'
-import { fileURLToPath, URL } from 'url'
+import fsPromise from 'fs/promises';
+import { join } from 'path';
+import { fileURLToPath, URL } from 'url';
 
-const __dirname = fileURLToPath(new URL('.', import.meta.url))
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 const rename = async () => {
-    const dirContent = await fsPromise.readdir(join(__dirname, 'files'))
-    if (dirContent.includes('properFilename.md'))
-        throw new Error('FS operation failed')
-    fsPromise.rename(
-        join(__dirname, 'files', 'wrongFilename.txt'),
-        join(__dirname, 'files', 'properFilename.md')
-    )
-}
+  const dirContent = await fsPromise.readdir(join(__dirname, 'files'));
+  if (dirContent.includes('properFilename.md')) throw new Error('FS operation failed');
+  fsPromise.rename(
+    join(__dirname, 'files', 'wrongFilename.txt'),
+    join(__dirname, 'files', 'properFilename.md')
+  );
+};
 
-await rename()
+await rename();
